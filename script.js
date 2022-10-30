fccf436506f9165b204b5596b1543f6d836510f7
datakvitsiani <datuka.colt@gmail.com>
2022-10-30 21:16:08 +0400

    finished

---
+++ script.js
@@ -0,0 +1,67 @@
+"use strict";
+
+// let user = {
+//     firstname: "giorgi",
+//     lastname: "smith",
+//     age: 25,
+//     studentstatus: "active"
+//   };
+
+//   console.log(user['studentstatus']);
+
+  // for cycle
+
+  // let names = ['irakli', 'davit', 'levani', 'giorgi', 'ani'];
+
+  // for (let x = 0; x < names.length; x++) {
+  //   console.log(names[x])
+  // }
+
+  // for (let y of names) {
+  //     console.log(y);
+  // }
+
+  // let z = 0;
+  // while (z < names.length) {
+  //   console.log(names[z]);
+  //   z++; 
+  // }
+
+  // let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+
+  // for (let x = 0; x < numbers.length; x++) {
+  //     if(numbers[x] > 5) {
+  //       console.log(numbers[x]);
+  //     }
+  // }
+
+  // if statement
+  
+//   let user = {
+//     name: 'giorgi',
+//     age:  20,
+//     studentstatus: 'active'
+// }
+
+// if (user['age'] < 18 && user['studentstatus'] == active) {
+//   console.log('Hello');
+// } else if(user['name'] = 'Levani' ) {
+//   console.log('Hello Levani');
+// } else if(user['studentstatus'] == active || user['age'] < 25) {
+//   console.log('Hello World');
+// } else {
+//   console.log('error');
+// }
+
+
+// let array = [ 2, 3, 5, 10,25,24,11, 100 , 6,7, 10 ]
+
+// for (let y of array) {
+// if (y % 2 != 1) {
+//   console.log(y);
+// }
+//   if (y % 2 != 0) {
+//     console.log(y);
+//   }
+// }
+
